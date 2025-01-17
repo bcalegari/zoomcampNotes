607859c7a22edc827aad4f3202213206c574b1bd
Bruno Calegari <bruno.calegas@gmail.com>
2025-01-17 19:42:29 +0000

    terraform files

--- learning/terraform/terraform_basic/main.tf
+++ learning/terraform/terraform_basic/main.tf
@@ -1,24 +1,24 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "4.51.0"
+      source = "hashicorp/google"
+      version = "6.16.0"
     }
   }
 }
 
 provider "google" {
 # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-#  credentials = 
-  project = "<Your Project ID>"
-  region  = "us-central1"
+#  credentials = "where key????????"
+  project = "learning-v001"
+  region  = "europe-southwest1"
 }
 
-
-
-resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "<Your Unique Bucket Name>"
-  location      = "US"
+# 
+# 
+resource "google_storage_bucket" "learn-bucket" {
+  name          = "learning-v001-bucket"
+  location      = "EUROPE-WEST1"
 
   # Optional, but recommended settings:
   storage_class = "STANDARD"
@@ -39,10 +39,10 @@ resource "google_storage_bucket" "data-lake-bucket" {
 
   force_destroy = true
 }
-
-
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "<The Dataset Name You Want to Use>"
-  project    = "<Your Project ID>"
-  location   = "US"
-}
+# 
+# 
+# resource "google_bigquery_dataset" "dataset" {
+#   dataset_id = "<The Dataset Name You Want to Use>"
+#   project    = "<Your Project ID>"
+#   location   = "US"
+# }
